67826c94cd6cf053112ac8ad55cdbf1f93f58a88
sumit vishwakarma <106902421+vsumit401@users.noreply.github.com>
2023-09-30 13:43:39 +0530

    This is pythone class

---
+++ fycs B .py
@@ -0,0 +1,8 @@
+#Python program to read and print username.
+
+#take input from user
+fname=input("Enter your First name:")
+lname=input("Enter your Last name:")
+#concatinating string("Your name is")with variable(name)
+#I have used Escape sequence character /t stands for tab.
+print("Your name is /t"  +  fname + lame)
